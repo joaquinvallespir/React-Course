727273058743dc509e2c0df4f1c21e939ffc3869
joaquinvallespir <joaquinvallespir@gmail.com>
2024-04-23 20:44:01 -0300

    class components 2

--- React-Course/adopt-me/src/Carousel.jsx
+++ React-Course/adopt-me/src/Carousel.jsx
@@ -2,21 +2,29 @@ import { Component } from "react";
 //class components
 class Carousel extends Component {
     state = {
-        active: 0
+        active: 0 //la foto activa por defecto
     }
     static defaultProps = {
-        images: ["http://pets-v2.dev-apis.com/pets/none.jpg"]
+        images: ["http://pets-v2.dev-apis.com/pets/none.jpg"] //placeholder
     }
-    render (){
-        const { active} = this.state
-        const { images} = this.props
-        console.log(images)
+
+    handleIndexClick = (e) =>{ //se usa una funcion flecha porque captura el contexto del evento
+        this.setState({
+            active: +e.target.dataset.index //el + convierte el string del index en un numero
+        })
+    }
+    render (){ //render  es obligatorio para cada class component
+        const { active} = this.state //equivalente a useState
+        const { images} = this.props //props son los parametros que se pasan al llamar la funcion, equivalente a useParams()
         return (
             <div className='carousel'>
                 <img src={images[active]} alt="animal hero"/>
                 <div className='carousel-smaller'>
                     {images.map((photo, index) => (
+                    // eslint-disable-next-line
                     <img
+                        onClick={this.handleIndexClick}
+                        data-index={index}
                         key={photo}
                         src={photo}
                         className={index === active ? "active": ""}
---
+++ teoria/js/pop.js
@@ -0,0 +1,4 @@
+var array = ["mauro", "mauro1", "mauro2", "mauro3"];
+array.shift();
+console.log(array);
+console.log(array.shift());
